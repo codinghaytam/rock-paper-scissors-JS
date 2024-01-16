f5174726087f7366182717cc770337d022e57d98
codinghaytam <haytams2210@gmail.com>
2024-01-16 15:09:04 +0100

    reorganizing files into folders and adding a css styling file

---
+++ scripts/script.js
@@ -0,0 +1,79 @@
+function getmachinechoice()
+{
+    let input=Math.round(Math.random()*2);
+    switch (input){
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+        default :
+            return "ERROR";
+    }
+}
+function getplayerchoice()
+{
+    let input= prompt("what is your choice {'Rock','Paper','Scissors'}");
+    if(input===null)input="";
+    switch (input.toLowerCase())
+    {
+        case "rock":
+            return "Rock";
+            break;
+        case "paper":
+            return "Paper";
+            break;
+        case "scissors":
+            return "Scissors";
+            break;
+        default :
+            return "ERROR";
+    }
+}
+function PlayRound(machine,player)
+{
+    let output;
+    if(player===machine) output="TIE";
+    else if (player=="Scissors")
+        {
+            if (machine=="Paper") output="Win";
+            else output="Lose";
+        }
+    else if (player=="Rock")
+        {
+            if (machine=="Scissors") output="Win";
+            else output="Lose";
+        }
+    else if (player=="Paper")
+        {
+            if (machine=="Rock") output="Win";
+            else output="Lose";
+        }
+    return ("You "+output);
+}
+function PlayMatch()
+{   
+    let player_score=0,machine_score=0
+    for (i=0;i<5;i++)
+    {
+        let result_round=PlayRound(getmachinechoice(),getplayerchoice());
+        if (result_round == "You Win")
+        {   player_score++;
+            console.log(result_round);
+        }
+        else if (result_round=="You Lose")
+        {
+            machine_score++;
+            console.log(result_round);
+
+        }
+        else {i--;console.log("Retry");}
+    }
+    if (player_score>machine_score)console.log("You Won the match");
+    else if (player_score<machine_score)console.log("You lost the match"); 
+    else console.log("Its a Tie"); 
+}
