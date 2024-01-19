72d0917663f225760ed1129d844f24d234f0b728
codinghaytam <112352396+codinghaytam@users.noreply.github.com>
2024-01-19 16:04:25 +0100

    deleting the unused functions getplayerchoice and playmatch

--- scripts/script.js
+++ scripts/script.js
@@ -16,25 +16,7 @@ function getmachinechoice()
             return "ERROR";
     }
 }
-function getplayerchoice(input)
-{
 
-    if(input===null)input="";
-    switch (input.toLowerCase())
-    {
-        case "rock":
-            return "Rock";
-            break;
-        case "paper":
-            return "Paper";
-            break;
-        case "scissors":
-            return "Scissors";
-            break;
-        default :
-            return "ERROR";
-    }
-}
 function PlayRound(machine,player)
 {
     let output;
@@ -56,28 +38,7 @@ function PlayRound(machine,player)
         }
     return ("You "+output);
 }
-function PlayMatch()
-{   
-    let player_score=0,machine_score=0
-    for (i=0;i<5;i++)
-    {
-        let result_round=PlayRound(getmachinechoice(),getplayerchoice());
-        if (result_round == "You Win")
-        {   player_score++;
-            console.log(result_round);
-        }
-        else if (result_round=="You Lose")
-        {
-            machine_score++;
-            console.log(result_round);
 
-        }
-        else {i--;console.log("Retry");}
-    }
-    if (player_score>machine_score)console.log("You Won the match");
-    else if (player_score<machine_score)console.log("You lost the match"); 
-    else console.log("Its a Tie"); 
-}
 
 const controles=Array.from(document.querySelectorAll("ul > img")).concat(document.querySelector("div > button"));
 const player_controles = controles.slice(0,3);
